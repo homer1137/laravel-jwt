5b9068487720e8b8de0928bbe8fd496ad4943360
Romango <homer37@yandex.ru>
2022-10-30 18:29:41 +0400

    поправил доступ к кукам

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Auth\Passwords\PasswordBroker;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Validation\Rules\Password;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|email',
+            'password' => ['required', 'string', Password::min(5)->letters()->numbers()->mixedCase()]
+        ]);
+        if($validator->fails()){
+           return response()->json($validator->errors()->toJson(),400);
+        }
+
+
         $user = User::create([
             'name'=>$request->input('name'),
             'email'=>$request->input('email'),
@@ -31,7 +44,7 @@ class AuthController extends Controller
        }
 
        $user = Auth::user();
-
+   /** @var \App\Models\MyUserModel $user **/
        $token =$user->createToken('token')->plainTextToken;
 
        $cookie = cookie('jwt', $token, 60*24);
